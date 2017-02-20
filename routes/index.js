f25951beee40fe30f9735fc968fcd23fcab61ae7
OrPerets <or@1trn.com>
2017-02-20 12:40:12 +0200

    add bluebird module for Promise

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,36 @@ var express = require('express');
 var router = express.Router();
 var mongoskin = require('mongoskin');
 var db = mongoskin.db('mongodb://localhost:27017/forum');
+db.bind('system', {});
 var ObjectID = require('mongoskin').ObjectID;
 var post = require('../models/Posts');
 var comment = require('../models/Comments');
-
-router.param('post', function(req, res, next, id) {
-  var query = db.collection('system').find(id).then(function(err, post) {
-    if (err) return next(err);
-    if (!post) return next(new Error('cant find post'));
-    req.post = post;
-    return next();
-  });
-});
+var Promise = require("bluebird");
 
 router.get('/posts/:post', function(req, res) {
   res.json(req.post);
 });
 
 router.get('/posts', function(req, res, next) {
-  return new Promise(function(fulfill, reject) {
-  db.collection('system').find().toArray(function(err, result) {
-    if (err) reject(err);
-    fulfill();
-  })
-}).then(res.json(result));
+  return Promise.try(function() {
+    db.collection('system').find().toArray(function(err, result) {
+    if (err) return next(err);
+  });
+}).then(function(result){
+  console.log("success");
+  res.json(result);
+});
+});
 
 router.post('/posts', function(req, res, next) {
   var newPost = new post(req.body);
-  return new Promise(function(fulfill, reject) {
+  return Promise.try(function(){
     db.collection('system').insert(newPost, function(err, result) {
-    if (err) reject(err);
-  })
-  }).then(console.log("added"));
+      if (err) return next(err);
+    });
+  }).then(function(result) {
+    if (result) console.log("added to DB");
+  });
 });
 
 /* GET home page. */
@@ -41,4 +39,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = router
+
+
